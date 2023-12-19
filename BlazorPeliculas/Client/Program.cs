a0eabc01fcb0c6cae3135541f1bd3ec5e4397d41
YeanOrma <german.andres201@gmail.com>
2023-12-19 16:25:48 -0500

    Aplicando inyección de dependencia de DialogService

--- BlazorPeliculas/Client/Program.cs
+++ BlazorPeliculas/Client/Program.cs
@@ -18,6 +18,7 @@ await builder.Build().RunAsync();
 void ConfigureServices(IServiceCollection services){
     services.AddSingleton<ServiciosSingleton>();
     services.AddTransient<ServiciosTransient>();
+    services.AddScoped<DialogService>();
     services.AddSweetAlert2();
     services.AddRadzenComponents();
     services.AddSingleton<IRepositorio, Repositorio>();
