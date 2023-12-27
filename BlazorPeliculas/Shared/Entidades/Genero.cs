1937fefb1ab0fa99a074841ac94300f39d14fca0
YeanOrma <german.andres201@gmail.com>
2023-12-27 11:03:46 -0500

    Diciendo que campos son obligatorios con su mensaje de Error

--- BlazorPeliculas/Shared/Entidades/Genero.cs
+++ BlazorPeliculas/Shared/Entidades/Genero.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorPeliculas.Shared.Entidades
 {
     public class Genero
     {
         public int Id { get; set; }
+        [Required (ErrorMessage = "El campo {0} es requerido")]
         public string Nombre { get; set; }
     }
 }
