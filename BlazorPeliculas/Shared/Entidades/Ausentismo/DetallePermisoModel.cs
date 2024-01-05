7901c876ecb3b59614b0562833450fbd436759cc
YeanOrma <germ.ortiz@udla.edu.co>
2024-01-05 17:53:10 -0500

    Creando el modelo para visualizar el permiso

---
+++ BlazorPeliculas/Shared/Entidades/Ausentismo/DetallePermisoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Shared.Entidades.Ausentismo
+{
+    public class DetallePermisoModel
+    {
+        public int Id { get; set; }
+        public string Dependencia { get; set; }
+        public string Motivo { get; set; }
+        public string Justificacion { get; set; }
+        public DateTime FechaDesde { get; set; }
+        public DateTime FechaHasta { get; set; }
+    }
+}
