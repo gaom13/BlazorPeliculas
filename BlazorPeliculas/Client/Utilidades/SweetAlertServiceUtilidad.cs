2017ebbc298f37840748db6c96b4a60085a1073a
YeanOrma <germ.ortiz@udla.edu.co>
2024-01-05 19:25:13 -0500

    Creando una clase de utilidad para SweetAlertServic

---
+++ BlazorPeliculas/Client/Utilidades/SweetAlertServiceUtilidad.cs
@@ -0,0 +1,27 @@
+﻿using BlazorPeliculas.Shared.Entidades.Ausentismo;
+using CurrieTechnologies.Razor.SweetAlert2;
+
+namespace BlazorPeliculas.Client.Utilidades
+{
+    public class SweetAlertServiceUtilidad
+    {
+        private readonly SweetAlertService _sweetAlertService;
+
+        public SweetAlertServiceUtilidad(SweetAlertService sweetAlertService)
+        {
+            _sweetAlertService = sweetAlertService;
+        }
+        public async Task<bool> Confirmacion(string title, string body)
+        {
+            var resultado = await _sweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = title,
+                Text = body,
+                Icon = SweetAlertIcon.Warning,
+                ShowCancelButton = true
+            });
+
+            return !string.IsNullOrEmpty(resultado.Value);
+        }
+    }
+}
