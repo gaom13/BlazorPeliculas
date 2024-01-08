41707704111fbd1f6faf75e2796803e383659a80
YeanOrma <germ.ortiz@udla.edu.co>
2024-01-07 22:10:20 -0500

    Creando modelos para consultar el hIstorial de los permisos del usuario

---
+++ BlazorPeliculas/Shared/Entidades/Ausentismo/HistorialPermisoConsultaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Shared.Entidades.Ausentismo
+{
+    public class HistorialPermisoConsultaModel
+    {
+        public string PgId { get; set; }
+        public DateTime Desde { get; set; } = DateTime.Now;
+        public DateTime Hasta { get; set; } = DateTime.Now;
+    }
+}
---
+++ BlazorPeliculas/Shared/Entidades/Ausentismo/HistorialPermisoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Shared.Entidades.Ausentismo
+{
+    public class HistorialPermisoModel
+    {
+        public string Estado { get; set; }
+        public string Descripcion { get; set; }
+        public DateOnly Desde { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public DateOnly Hasta { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public int Hora { get; set; }
+    }
+}
