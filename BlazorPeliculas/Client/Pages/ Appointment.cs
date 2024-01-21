823bf510de8cba2c8dbd29e3fbbef6bd22d94c6c
YeanOrma <germ.ortiz@udla.edu.co>
2024-01-21 11:06:12 -0500

    Cambiando el modelo del Calendario

--- BlazorPeliculas/Client/Pages/ Appointment.cs
+++ BlazorPeliculas/Client/Pages/ Appointment.cs
@@ -1,12 +1,18 @@
+using BlazorPeliculas.Shared.Entidades.Ausentismo;
 using System;
 
 namespace BlazorPeliculas.Client.Pages
 {
     public class Appointment
     {
+        public int Id { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
 
         public string Text { get; set; } = null!;
+        public string Dependencia { get; set; }
+        public string Motivo { get; set; }
+        public string Justificacion { get; set; }
+        public List<DetallePermisoJefeModel> PermisoJefeModel { get; set; }
     }
 }
