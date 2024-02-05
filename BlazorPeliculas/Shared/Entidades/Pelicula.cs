07e60680e81ff06f8f0ff74c7ee4ed4d0944d1ed
YeanOrma <germ.ortiz@udla.edu.co>
2024-02-05 10:26:23 -0500

    Modificando la entidad Pelicula

--- BlazorPeliculas/Shared/Entidades/Pelicula.cs
+++ BlazorPeliculas/Shared/Entidades/Pelicula.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,14 @@ namespace BlazorPeliculas.Shared.Entidades
 {
     public class Pelicula
     {
-        public string? Titulo { get; set; }
-        public DateTime FechaLanzamiento { get; set; }
-        public string Poster { get; set; } = null! ;
+        public int Id { get; set; }
+        [Required]
+        public string Titulo { get; set; } = null!;
+        public string? Resumen { get; set; }
+        public bool EnCartelera { get; set; }
+        public string Trailer { get; set; }
+        public DateTime? Lanzamiento { get; set; }
+        public string? Poster { get; set; }
         public string TituloCortado { 
             get{
                 if(string.IsNullOrWhiteSpace(Titulo)){
